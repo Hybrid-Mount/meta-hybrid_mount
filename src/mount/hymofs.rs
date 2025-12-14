89b2886e6ff4a97ef3756bc2a1a89cba7626933e
YuzakiKokuban <heibanbaize@gmail.com>
2025-12-14 14:14:07 +0800

    Fix: Resolve compilation errors by using libc for ioctl and correcting c_char types for aarch64

--- src/mount/hymofs.rs
+++ src/mount/hymofs.rs
@@ -10,17 +10,47 @@ use crate::defs::HYMO_PROTOCOL_VERSION;
 const HYMO_DEV: &str = "/dev/hymo_ctl";
 const HYMO_IOC_MAGIC: u8 = 0xE0;
 
-nix::ioctl_write_ptr!(hymo_add_rule, HYMO_IOC_MAGIC, 1, HymoIoctlArg);
-nix::ioctl_write_ptr!(hymo_del_rule, HYMO_IOC_MAGIC, 2, HymoIoctlArg);
-nix::ioctl_write_ptr!(hymo_hide_rule, HYMO_IOC_MAGIC, 3, HymoIoctlArg);
-nix::ioctl_none!(hymo_clear_all, HYMO_IOC_MAGIC, 5);
-nix::ioctl_read!(hymo_get_version, HYMO_IOC_MAGIC, 6, i32);
-nix::ioctl_write_ptr!(hymo_set_debug, HYMO_IOC_MAGIC, 8, i32);
+const _IOC_NRBITS: u32 = 8;
+const _IOC_TYPEBITS: u32 = 8;
+const _IOC_SIZEBITS: u32 = 14;
+const _IOC_DIRBITS: u32 = 2;
+
+const _IOC_NRSHIFT: u32 = 0;
+const _IOC_TYPESHIFT: u32 = _IOC_NRSHIFT + _IOC_NRBITS;
+const _IOC_SIZESHIFT: u32 = _IOC_TYPESHIFT + _IOC_TYPEBITS;
+const _IOC_DIRSHIFT: u32 = _IOC_SIZESHIFT + _IOC_SIZEBITS;
+
+const _IOC_NONE: u32 = 0;
+const _IOC_WRITE: u32 = 1;
+const _IOC_READ: u32 = 2;
+
+const fn _ioc(dir: u32, type_: u32, nr: u32, size: u32) -> u32 {
+    (dir << _IOC_DIRSHIFT) | (type_ << _IOC_TYPESHIFT) | (nr << _IOC_NRSHIFT) | (size << _IOC_SIZESHIFT)
+}
+
+const fn _io(type_: u32, nr: u32) -> u32 {
+    _ioc(_IOC_NONE, type_, nr, 0)
+}
+
+const fn _ior(type_: u32, nr: u32, size: usize) -> u32 {
+    _ioc(_IOC_READ, type_, nr, size as u32)
+}
+
+const fn _iow(type_: u32, nr: u32, size: usize) -> u32 {
+    _ioc(_IOC_WRITE, type_, nr, size as u32)
+}
+
+const HYMO_IOC_ADD_RULE: u64 = _iow(HYMO_IOC_MAGIC as u32, 1, std::mem::size_of::<HymoIoctlArg>()) as u64;
+const HYMO_IOC_DEL_RULE: u64 = _iow(HYMO_IOC_MAGIC as u32, 2, std::mem::size_of::<HymoIoctlArg>()) as u64;
+const HYMO_IOC_HIDE_RULE: u64 = _iow(HYMO_IOC_MAGIC as u32, 3, std::mem::size_of::<HymoIoctlArg>()) as u64;
+const HYMO_IOC_CLEAR_ALL: u64 = _io(HYMO_IOC_MAGIC as u32, 5) as u64;
+const HYMO_IOC_GET_VERSION: u64 = _ior(HYMO_IOC_MAGIC as u32, 6, std::mem::size_of::<i32>()) as u64;
+const HYMO_IOC_SET_DEBUG: u64 = _iow(HYMO_IOC_MAGIC as u32, 8, std::mem::size_of::<i32>()) as u64;
 
 #[repr(C)]
 struct HymoIoctlArg {
-    src: *const i8,
-    target: *const i8,
+    src: *const libc::c_char,
+    target: *const libc::c_char,
     type_: i32,
 }
 
@@ -62,25 +92,35 @@ impl HymoFs {
     pub fn get_version() -> Option<i32> {
         let file = File::open(HYMO_DEV).ok()?;
         let mut version: i32 = 0;
-        unsafe {
-            hymo_get_version(file.as_raw_fd(), &mut version).ok()?;
+        let ret = unsafe {
+            libc::ioctl(file.as_raw_fd(), HYMO_IOC_GET_VERSION, &mut version)
+        };
+        if ret == 0 {
+            Some(version)
+        } else {
+            None
         }
-        Some(version)
     }
 
     pub fn set_debug(enable: bool) -> Result<()> {
         let file = File::open(HYMO_DEV)?;
         let val: i32 = if enable { 1 } else { 0 };
-        unsafe {
-            hymo_set_debug(file.as_raw_fd(), &val)?;
+        let ret = unsafe {
+            libc::ioctl(file.as_raw_fd(), HYMO_IOC_SET_DEBUG, &val)
+        };
+        if ret != 0 {
+            anyhow::bail!("Failed to set debug mode, ioctl ret: {}", ret);
         }
         Ok(())
     }
 
     pub fn clear() -> Result<()> {
         let file = File::open(HYMO_DEV).context("Failed to open HymoFS control device")?;
-        unsafe {
-            hymo_clear_all(file.as_raw_fd())?;
+        let ret = unsafe {
+            libc::ioctl(file.as_raw_fd(), HYMO_IOC_CLEAR_ALL)
+        };
+        if ret != 0 {
+            anyhow::bail!("Failed to clear rules, ioctl ret: {}", ret);
         }
         Ok(())
     }
@@ -96,8 +136,11 @@ impl HymoFs {
             type_,
         };
 
-        unsafe {
-            hymo_add_rule(file.as_raw_fd(), &arg)?;
+        let ret = unsafe {
+            libc::ioctl(file.as_raw_fd(), HYMO_IOC_ADD_RULE, &arg)
+        };
+        if ret != 0 {
+            anyhow::bail!("Failed to add rule, ioctl ret: {}", ret);
         }
         Ok(())
     }
@@ -112,8 +155,11 @@ impl HymoFs {
             type_: 0,
         };
 
-        unsafe {
-            hymo_hide_rule(file.as_raw_fd(), &arg)?;
+        let ret = unsafe {
+            libc::ioctl(file.as_raw_fd(), HYMO_IOC_HIDE_RULE, &arg)
+        };
+        if ret != 0 {
+            anyhow::bail!("Failed to hide path, ioctl ret: {}", ret);
         }
         Ok(())
     }
