b888caee8d09614ca86e95f732d376971ca11dee
Tools-app <localhost.hutao@gmail.com>
2026-01-31 10:32:51 +0800

    fix: fix no deleted base dir when uninstall

---
+++ module/uninstall.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+############################################
+# mix-mount uninstall.sh
+# Cleanup script for metamodule removal
+############################################
+
+rm -rf "/data/adb/meta-hybrid"
+
+exit 0
